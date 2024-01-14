2aa2be590f81225a218f2a7e03708703ddea4776
asilvaolabi <asilva@olabi.org.br>
2024-01-14 13:43:15 -0300

    Validating creation of users with unique emails

--- src/dto/CriaUsuario.dto.ts
+++ src/dto/CriaUsuario.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { EmailEhUnico } from 'src/usuario/validacao/email-eh-unico.validator';
 
 export class CriaUsuarioDTO {
     @IsNotEmpty({message: 'O nome não pode ser vazio!'})
     nome: string;
 
     @IsEmail(undefined, {message: 'O e-mail informado é inválido!'})
+    @EmailEhUnico({message: 'Já existe um usuário com esse e-mail'})
     email: string;
 
     @MinLength(6, {message: 'A senha precisa ter no mínimo 06 caracteres!'})
